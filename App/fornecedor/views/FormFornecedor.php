5fb9dab44d96195276319e1685dae410b139afe7
Cesar Buchile <buchile.cesar@gmail.com>
2023-01-24 15:44:04 -0200

    formulários estilizados com bootstrap

--- App/fornecedor/views/FormFornecedor.php
+++ App/fornecedor/views/FormFornecedor.php
@@ -5,37 +5,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="/statics/js/masks.js"></script>
-    <link rel="stylesheet" type="text/css" href="/statics/css/FormFornecedor.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/statics/css/GeneralForm.css">
     <title>Fornecedor</title>
 </head>
 <body>
-    <fieldset>
-        <legend>Cadastro de Fornecedor</legend>
+    <fieldset class="shadow px-5 py-5 rounded-4">
+        <legend class="mb-3">Cadastro de Fornecedor</legend>
         <form method="POST" action="/fornecedor/form/save">
-           
-            <?php if(isset($_GET["next"])) :?>
-            <input type="hidden" value="<?= $_GET["next"] ?>" name="next">
-            <?php endif ?>                
-        
-            <input type="hidden" value="<?= $model->id_fornecedor ?>" name="id_fornecedor">
-            <label for="cnpj">CNPJ do fornecedor</label>
-            <input id="cnpj" name="cnpj" value="<?= $model->cnpj ?>" type="text" onkeyup="maskCNPJ(this);" maxlength="20">
-            <label for="razao_social">Razão social</label>
-            <input id="razao_social" name="razao_social" value="<?= $model->razao_social ?>" type="text" onkeyup="maskNameUpper(this);" maxlength="255">
-            <label for="nome_fantasia">Nome fantasia</label>
-            <input id="nome_fantasia" name="nome_fantasia" value="<?= $model->nome_fantasia ?>" type="text" onkeyup="maskNameUpper(this);" maxlength="255">
-            <label for="telefone">Telefone do fornecedor</label>
-            <input id="telefone" name="telefone" value="<?= $model->telefone ?>" type="text" onkeyup="maskCellPhone(this);" maxlength="20">
-            <label for="nome_vendedor">Nome do vendedor</label>
-            <input id="nome_vendedor" name="nome_vendedor" value="<?= $model->nome_vendedor ?>" type="text" onkeyup="maskName(this);" maxlength="255">
-            <label for="email_vendedor">E-mail do vendedor</label>
-            <input id="email_vendedor" name="email_vendedor" value="<?= $model->email_vendedor ?>" type="text" onkeyup="maskEmail(this);" maxlength="255">
-            <label for="celular_vendedor">Celular do vendedor</label>
-            <input id="celular_vendedor" name="celular_vendedor" value="<?= $model->celular_vendedor ?>" type="text" onkeyup="maskCellPhone(this);" maxlength="20">
-            <div buttons>
-                <a href="<?= isset($_GET["next"]) ? "/".$_GET["next"] : "/fornecedores"; ?>"><button type="button">voltar</button></a>
-                <button type="submit">salvar</button>                
+            <div class="form-group">
+                <?php if(isset($_GET["next"])) :?>
+                <input type="hidden" value="<?= $_GET["next"] ?>" name="next">
+                <?php endif ?>                
+            </div>
+            <div class="form-group">
+                <input type="hidden" value="<?= $model->id_fornecedor ?>" name="id_fornecedor">
+            </div>
+            <div class="form-group">
+                <label for="cnpj">CNPJ do fornecedor</label>
+                <input class="form-control" id="cnpj" name="cnpj" value="<?= $model->cnpj ?>" type="text" onkeyup="maskCNPJ(this);" maxlength="20">
+            </div>
+            <div class="form-group mt-2">
+                <label for="razao_social">Razão social</label>
+                <input class="form-control" id="razao_social" name="razao_social" value="<?= $model->razao_social ?>" type="text" onkeyup="maskNameUpper(this);" maxlength="255">
+            </div>
+            <div class="form-group mt-2">
+                <label for="nome_fantasia">Nome fantasia</label>
+                <input class="form-control" id="nome_fantasia" name="nome_fantasia" value="<?= $model->nome_fantasia ?>" type="text" onkeyup="maskNameUpper(this);" maxlength="255">
+            </div>        
+            <div class="form-group mt-2">
+                <label for="telefone">Telefone do fornecedor</label>
+                <input class="form-control" id="telefone" name="telefone" value="<?= $model->telefone ?>" type="text" onkeyup="maskCellPhone(this);" maxlength="20">
+            </div>
+            <div class="form-group mt-2">
+                <label for="nome_vendedor">Nome do vendedor</label>
+                <input class="form-control" id="nome_vendedor" name="nome_vendedor" value="<?= $model->nome_vendedor ?>" type="text" onkeyup="maskName(this);" maxlength="255">
+            </div>
+            <div class="form-group mt-2">
+                <label for="email_vendedor">E-mail do vendedor</label>
+                <input class="form-control" id="email_vendedor" name="email_vendedor" value="<?= $model->email_vendedor ?>" type="text" onkeyup="maskEmail(this);" maxlength="255">
+            </div>
+            <div class="form-group mt-2">
+                <label for="celular_vendedor">Celular do vendedor</label>
+                <input class="form-control" id="celular_vendedor" name="celular_vendedor" value="<?= $model->celular_vendedor ?>" type="text" onkeyup="maskCellPhone(this);" maxlength="20">
+            </div>        
+            <div class="btn-group mt-4" role="group">
+                <a class="btn btn-dark me-1" href="<?= isset($_GET["next"]) ? "/".$_GET["next"] : "/fornecedores"; ?>">voltar</a>
+                <button class="btn btn-primary" type="submit">salvar</button>                
             </div>
         </form>
     </fieldset>
--- App/produto/views/FormProduto.php
+++ App/produto/views/FormProduto.php
@@ -5,39 +5,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="/statics/js/masks.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/statics/css/GeneralForm.css">
     <title>Produto</title>
 </head>
 <body>
-    <fieldset>
-        <legend>Cadastro de Produtos</legend>
-        <form method="POST" action="/produto/form/save">
-            
-            <input type="hidden" value="<?= $model->id_produto ?>" name="id_produto">
-            
-            <label for="id_fornecedor">Fornecedor</label>
-            <select id="id_fornecedor" name="id_fornecedor">
-            <?php foreach($model->fornecedores as $item) :?>
-                <?php if($item->id_fornecedor == $model->id_fornecedor):?>
-                <option value="<?= $item->id_fornecedor?>" selected><?= $item->nome_fantasia ?></option>
-                <?php else :?>
-                <option value="<?= $item->id_fornecedor?>"><?= $item->nome_fantasia ?></option>
-                <?php endif ?>
-            <?php endforeach ?>    
-            </select>
-            
-            
-            <label for="nome_produto">Produto</label>
-            <input id="nome_produto" name="nome_produto" value="<?= $model->nome_produto ?>" type="text" onkeyup="maskName(this);">
-            <label for="valor_produto">Valor (em R$)</label>
-            <input id="valor_produto" name="valor_produto" value="<?= $model->valor_produto ?>" type="text" onkeyup="maskDecimal(this);">
-            <label for="peso">Peso (em gramas)</label>
-            <input id="peso" name="peso" value="<?= $model->peso ?>" type="text" onkeyup="maskDecimal(this);">
-            <label for="quantidade_estoque">Qtd. no estoque</label>
-            <input id="quantidade_estoque" name="quantidade_estoque" value="<?= $model->quantidade_estoque ?>" type="text" onkeyup="maskPositiveInteger(this);">
-            <div buttons>
-                <a href="<?= isset($_GET["next"]) ? "/".$_GET["next"] : "/produtos"; ?>"><button type="button">voltar</button></a>
-                <button type="submit">salvar</button>                
+    <fieldset class="shadow px-5 py-5 rounded-4">
+        <legend class="mb-3">Cadastro de Produtos</legend>
+        <form method="POST" action="/produto/form/save">            
+            <div class="form-group">    
+                <input class="form-control" type="hidden" value="<?= $model->id_produto ?>" name="id_produto">
+            </div>            
+            <div class="form-group">
+                <label for="id_fornecedor">Fornecedor</label>
+                <select class="form-select" id="id_fornecedor" name="id_fornecedor">
+                <?php foreach($model->fornecedores as $item) :?>
+                    <?php if($item->id_fornecedor == $model->id_fornecedor):?>
+                    <option value="<?= $item->id_fornecedor?>" selected><?= $item->nome_fantasia ?></option>
+                    <?php else :?>
+                    <option value="<?= $item->id_fornecedor?>"><?= $item->nome_fantasia ?></option>
+                    <?php endif ?>
+                <?php endforeach ?>    
+                </select>                
+            </div>
+            <div class="form-group mt-2">            
+                <label for="nome_produto">Produto</label>
+                <input class="form-control" id="nome_produto" name="nome_produto" value="<?= $model->nome_produto ?>" type="text" onkeyup="maskName(this);">
+            </div>
+            <div class="form-group mt-2">
+                <label for="valor_produto">Valor (em R$)</label>
+                <input class="form-control" id="valor_produto" name="valor_produto" value="<?= $model->valor_produto ?>" type="text" onkeyup="maskDecimal(this);">
+            </div>
+            <div class="form-group mt-2">
+                <label for="peso">Peso (em gramas)</label>
+                <input class="form-control" id="peso" name="peso" value="<?= $model->peso ?>" type="text" onkeyup="maskDecimal(this);">
+            </div>
+            <div class="form-group mt-2">
+                <label for="quantidade_estoque">Qtd. no estoque</label>
+                <input class="form-control" id="quantidade_estoque" name="quantidade_estoque" value="<?= $model->quantidade_estoque ?>" type="text" onkeyup="maskPositiveInteger(this);">
+            </div>
+            <div class="btn-group mt-4" role="group">
+                <a class="btn btn-dark me-1" href="<?= isset($_GET["next"]) ? "/".$_GET["next"] : "/produtos"; ?>">voltar</a>
+                <button type="submit" class="btn btn-primary">salvar</button>                
             </div>            
         </form>
     </fieldset>
