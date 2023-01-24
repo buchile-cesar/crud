c00967ae88ec443f52155f1da869923c9902fe4e
Cesar Buchile <buchile.cesar@gmail.com>
2023-01-24 15:44:52 -0200

    home estilizada com bootstrap

--- App/home/views/Home.php
+++ App/home/views/Home.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/statics/css/Home.css">
     <title>Home</title>
 </head>
 <body>
-    <div>
-        <a href="/fornecedores"><button type="button">Lista de Fornecedores</button></a>
-        <a href="/produtos"><button type="button">Lista de Produtos</button></a>
+    <div class="d-grid gap-2">
+        <a class="btn btn-dark" href="/fornecedores">Lista de Fornecedores</a>
+        <a class="btn btn-dark" href="/produtos">Lista de Produtos</a>
     </div>
 </body>
 </html>
